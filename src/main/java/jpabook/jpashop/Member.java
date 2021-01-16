adc3c69ab624f6b23608a6659dc2b762294b5a75
leesanghyun <practicelee@naver.com>
2021-01-16 17:33:03 +0900

    add: db test, TDD

--- src/main/java/jpabook/jpashop/Member.java
+++ src/main/java/jpabook/jpashop/Member.java
@@ -1,13 +1,15 @@
 package jpabook.jpashop;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Member {
 
     @Id
--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,10 +1,28 @@
 package jpabook.jpashop.repository;
 
+
+import jpabook.jpashop.Member;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 // DAO랑 비슷
 @Repository
 public class MemberRepository {
+
+    // 스프링부트가 엔티티매니저 주입을 해줌
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+     public Member find(Long id) {
+        return em.find(Member.class, id);
+     }
 
 
 }
